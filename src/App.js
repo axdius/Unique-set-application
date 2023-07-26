8d1caff8473656b58360a44ada76a0bd5a5c56bd
Prashant Kumar <124628326+axdius@users.noreply.github.com>
2023-07-27 00:11:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,80 +1,85 @@
-import React, { Component} from 'react';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   state = {
-    List1 : ['Arjun', 'Adwait', 'Swapnil','Amit', 'Vishal', 'Adnan'],
-    List2 : ['Adwait', 'Laxman','Amit','Adnan','Nitin','Gaurav'],
+    List1: ["Arjun", "Adwait", "Swapnil", "Amit", "Vishal", "Adnan"],
+    List2: ["Adwait", "Laxman", "Amit", "Adnan", "Nitin", "Gaurav"],
     uniqueSet1notIn2: [],
     uniqueSet2notIn1: [],
-    intersectionSet: []
-  }
+    intersectionSet: [],
+  };
 
   findUniqueSetsAndIntersection = () => {
-    const { List1, List2} = this.state
-    
-    const uniqueSet1notIn2 = List1.filter(item => !List2.includes(item));
+    const { List1, List2 } = this.state;
 
-    
-    const uniqueSet2notIn1 = List2.filter(item => !List1.includes(item));
+    const uniqueSet1notIn2 = List1.filter((item) => !List2.includes(item));
 
-    
-    const intersectionSet = List1.filter(item => List2.includes(item));
+    const uniqueSet2notIn1 = List2.filter((item) => !List1.includes(item));
+
+    const intersectionSet = List1.filter((item) => List2.includes(item));
 
     return {
       uniqueSet1notIn2,
       uniqueSet2notIn1,
-      intersectionSet
+      intersectionSet,
     };
+  };
 
- }
+  handleUniqueSetOfList1NotInList2 = () => {
+    const result = this.findUniqueSetsAndIntersection();
+    this.setState({ uniqueSet1notIn2: result.uniqueSet1notIn2 });
+  };
 
- handleFindUniqueSetsAndIntersection = () => {
-  const result = this.findUniqueSetsAndIntersection();
-  this.setState({
-    uniqueSet1notIn2: result.uniqueSet1notIn2,
-    uniqueSet2notIn1: result.uniqueSet2notIn1,
-    intersectionSet: result.intersectionSet
-  });
-};
+  handleUniqueSetOfList2NotInList1 = () => {
+    const result = this.findUniqueSetsAndIntersection();
+    this.setState({ uniqueSet2notIn1: result.uniqueSet2notIn1 });
+  };
 
-render() {
-  const { uniqueSet1notIn2, uniqueSet2notIn1, intersectionSet } = this.state;
+  handleIntersectionOfList1AndList2 = () => {
+    const result = this.findUniqueSetsAndIntersection();
+    this.setState({ intersectionSet: result.intersectionSet });
+  };
 
-  return (
-    <div className="App">
-      <h1>Unique Lists App</h1>
-      <button onClick={this.handleFindUniqueSetsAndIntersection}>Find Unique Sets and Intersection</button>
+  render() {
+    const { uniqueSet1notIn2, uniqueSet2notIn1, intersectionSet } = this.state;
 
-      <div>
-        <h2>Unique sets from list 1 not in list 2:</h2>
-        <ul>
-          {uniqueSet1notIn2.map(item => <li key={item}>{item}</li>)}
-        </ul>
+    return (
+      <div className="App">
+        <h1>Unique Lists App</h1>
+
+        <div>
+          <h2>Unique sets from list 1 not in list 2:</h2>
+          <button onClick={this.handleUniqueSetOfList1NotInList2}>Show</button>
+          <ul>
+            {uniqueSet1notIn2.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <h2>Unique sets from list 2 not in list 1:</h2>
+          <button onClick={this.handleUniqueSetOfList2NotInList1}>Show</button>
+          <ul>
+            {uniqueSet2notIn1.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <h2>Intersection of list 1 and list 2:</h2>
+          <button onClick={this.handleIntersectionOfList1AndList2}>Show</button>
+          <ul>
+            {intersectionSet.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </div>
       </div>
-
-      <div>
-        <h2>Unique sets from list 2 not in list 1:</h2>
-        <ul>
-          {uniqueSet2notIn1.map(item => <li key={item}>{item}</li>)}
-        </ul>
-      </div>
-
-      <div>
-        <h2>Intersection of list 1 and list 2:</h2>
-        <ul>
-          {intersectionSet.map(item => <li key={item}>{item}</li>)}
-        </ul>
-      </div>
-    </div>
-  );
+    );
+  }
 }
-}
-
-
-  
-
-  
 
 export default App;
