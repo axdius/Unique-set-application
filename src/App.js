7ac58ce7164b4d25533caee9c97fd763aef9bc0b
Prashant Kumar <124628326+axdius@users.noreply.github.com>
2023-07-26 21:08:46 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
 
-function App() {
+
+class App extends Component {
+  state = {
+    List1 : ['Arjun', 'Adwait', 'Swapnil','Amit', 'Vishal', 'Adnan'],
+    List2 : ['Adwait', 'Laxman','Amit','Adnan','Nitin','Gaurav'],
+    uniqueSet1notIn2: [],
+    uniqueSet2notIn1: [],
+    intersectionSet: []
+  }
+
+  findUniqueSetsAndIntersection = () => {
+    const { List1, List2} = this.state
+    
+    const uniqueSet1notIn2 = List1.filter(item => !List2.includes(item));
+
+    
+    const uniqueSet2notIn1 = List2.filter(item => !List1.includes(item));
+
+    
+    const intersectionSet = List1.filter(item => List2.includes(item));
+
+    return {
+      uniqueSet1notIn2,
+      uniqueSet2notIn1,
+      intersectionSet
+    };
+
+ }
+
+ handleFindUniqueSetsAndIntersection = () => {
+  const result = this.findUniqueSetsAndIntersection();
+  this.setState({
+    uniqueSet1notIn2: result.uniqueSet1notIn2,
+    uniqueSet2notIn1: result.uniqueSet2notIn1,
+    intersectionSet: result.intersectionSet
+  });
+};
+
+render() {
+  const { uniqueSet1notIn2, uniqueSet2notIn1, intersectionSet } = this.state;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Unique Lists App</h1>
+      <button onClick={this.handleFindUniqueSetsAndIntersection}>Find Unique Sets and Intersection</button>
+
+      <div>
+        <h2>Unique sets from list 1 not in list 2:</h2>
+        <ul>
+          {uniqueSet1notIn2.map(item => <li key={item}>{item}</li>)}
+        </ul>
+      </div>
+
+      <div>
+        <h2>Unique sets from list 2 not in list 1:</h2>
+        <ul>
+          {uniqueSet2notIn1.map(item => <li key={item}>{item}</li>)}
+        </ul>
+      </div>
+
+      <div>
+        <h2>Intersection of list 1 and list 2:</h2>
+        <ul>
+          {intersectionSet.map(item => <li key={item}>{item}</li>)}
+        </ul>
+      </div>
     </div>
   );
 }
+}
+
+
+  
+
+  
 
 export default App;
